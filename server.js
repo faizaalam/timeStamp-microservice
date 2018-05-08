b59f87417b658c0189300ef2da448f4da0d49d02
Glitch (timestamp-microservice-faiza) <none>
2018-05-08 15:32:37 +0000

    🛢🎅 Checkpoint ./server.js:1075162/714

--- server.js
+++ server.js
@@ -15,21 +15,12 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.send("https://timestamp-ms.herokuapp.com/1450137600");
 });
-app.get("/:date", function(req,res){
-// Create a new JavaScript Date object based on the timestamp
-// multiplied by 1000 so that the argument is in milliseconds, not seconds.
-var date = new Date(req.params.date*1000);
-// Hours part from the timestamp
-var hours = date.getHours();
-// Minutes part from the timestamp
-var minutes = "0" + date.getMinutes();
-// Seconds part from the timestamp
-var seconds = "0" + date.getSeconds();
-
-// Will display time in 10:30:23 format
-var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    res.send(date);
+app.get("/", function(req,res){
+  res.send('<p>Example usage:</p><code>http://localhost:8080/December%2015,%202015</code><br><code>http://localhost:8080/1450137600</code>');
 });
+
+app..
+
   
 
 // listen for requests :)
